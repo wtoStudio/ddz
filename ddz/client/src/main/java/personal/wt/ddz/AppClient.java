6c3d15620b83c3e8dbb9f2ab7785a43e87a686dc
wtoStudio <wantao_studio@163.com>
2020-01-08 09:06:01 +0800

    首次提交

--- ddz/client/src/main/java/personal/wt/ddz/AppClient.java
+++ ddz/client/src/main/java/personal/wt/ddz/AppClient.java
@@ -1,15 +1,14 @@
-package personal.wt.ddz.ui.personal.wt.ddz;
+package personal.wt.ddz;
 
+import personal.wt.ddz.ui.GamePanel;
 import personal.wt.ddz.ui.MainFrame;
 import javax.swing.*;
 
-/**
- * @author ttb
- */
-public class ClientApp {
+public class AppClient {
     public static void main(String[] args) throws ClassNotFoundException, UnsupportedLookAndFeelException, InstantiationException, IllegalAccessException {
         UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
-        MainFrame app = new MainFrame("关起门来斗地主");
-        app.getGamePanel().requestFocus();
+        MainFrame mainFrame = new MainFrame("斗地主");
+        GamePanel gamePanel = mainFrame.getGamePanel();
+        gamePanel.requestFocus();
     }
 }
