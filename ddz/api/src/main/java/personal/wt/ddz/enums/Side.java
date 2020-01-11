57cf79bf55d42773fe5c297febe7251b4363536b
wtoStudio <wantao_studio@163.com>
2020-01-11 09:31:53 +0800

    .

--- ddz/api/src/main/java/personal/wt/ddz/enums/Side.java
+++ ddz/api/src/main/java/personal/wt/ddz/enums/Side.java
@@ -1,11 +1,22 @@
 package personal.wt.ddz.enums;
 
 /**
+ * 相对位置枚举类
  * @author ttb
  */
-
 public enum Side {
-    LOCAL("LOCAL", "本家"), NEXT("NEXT", "下家"), PREV("PREV", "上家");
+    /**
+     * 本家
+     */
+    LOCAL("LOCAL", "本家"),
+    /**
+     * 下家
+     */
+    NEXT("NEXT", "下家"),
+    /**
+     * 上家
+     */
+    PREV("PREV", "上家");
 
     String code;
 
